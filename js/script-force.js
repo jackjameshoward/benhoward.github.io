53cbad1149bc51b4f205a3fb74af81087c716a1b
leighhopcroft <leighhopcroft@googlemail.com>
2017-04-05 21:30:11 +0100

    Update script-force.js

--- js/script-force.js
+++ js/script-force.js
@@ -6,7 +6,7 @@ var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", -1000)
+    .force("charge", d3.forceManyBody().strength(-30))
     .force("center", d3.forceCenter(width / 2, height / 2));
 
 d3.json("data.json", function(error, graph) {
